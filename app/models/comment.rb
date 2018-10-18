8742c4458df4035507afaf5136e4b68360ce59ff
ViachMoz <wanepain@gmail.com>
2018-10-18 21:15:10 +0300

    release the blogo

---
+++ app/models/comment.rb
@@ -0,0 +1,20 @@
+class Comment < ApplicationRecord
+  validates :author, presence: true,
+            length: { minimum: 2 }
+
+  validates :content, presence: true
+  validates_each :author do |record, attr, value|
+    record.errors.add(attr, 'must start with upper case ') if value =~ /\A[[:lower:]]/
+  end
+
+  validates_each :author do |record, attr, value|
+    record.errors.add(attr, 'second word must also start with upper case ') if value.split[1] =~ /\A[[:lower:]]/
+  end
+
+  validate :name_has_enough_words
+
+  def name_has_enough_words
+    return if author.blank?
+    errors.add(:author, "must be at least 2 words") if author.split.size < 2
+  end
+end
---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :post do
+    name "MyString"
+    content "MyString"
+  end
+end
